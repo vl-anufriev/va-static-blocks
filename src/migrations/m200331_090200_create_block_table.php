356501ac9194c03f9fe1f9de467811beabd344e1
Vladimir Olegovic <vladimirolegovic@MacPro.local>
2020-03-31 17:40:06 +0300

    Updated migration

--- src/migrations/m200331_090200_create_block_table.php
+++ src/migrations/m200331_090200_create_block_table.php
@@ -12,12 +12,17 @@ class m200331_090200_create_block_table extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
         $this->createTable('{{%block}}', [
             'id' => $this->primaryKey(),
             'title' => $this->string(),
             'slug' => $this->string(),
             'body' => $this->text(),
-        ]);
+        ], $tableOptions);
     }
 
     /**
